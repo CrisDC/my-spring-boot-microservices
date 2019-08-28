7aeeaf8bea8a0fca27c41cb091054cc505b1db01
CrisDC <cristhianw11@hotmail.com>
2019-08-28 16:35:27 -0500

    first commit

---
+++ movie-catalog-service/src/test/java/pe/edu/unmsm/fisi/moviecatalogservice/MovieCatalogServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.unmsm.fisi.moviecatalogservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieCatalogServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
